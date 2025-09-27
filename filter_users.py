d301818ee68b1563a8ba182e19e7b6cd66057bbf
jasonbdt <ja.bladt@gmail.com>
2025-09-27 19:08:16 +0200

    Refactore code to improve maintainablity

--- filter_users.py
+++ filter_users.py
@@ -1,37 +1,87 @@
 import json
+from typing import Any
+
+USERS_FILE = "users.json"
 
 
-def filter_users_by_name(name):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+def get_users() -> list[dict[str, Any]] | None:
+    """
+    Read ``users.json`` file and returns the values as dictionary.
+    Returns None if file was unable to load.
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    Raises:
+        FileNotFoundError: File with users not found.
 
-    for user in filtered_users:
+    Returns:
+        dict[str, Any]: A collection of users as dictionary.
+    """
+    try:
+        with open(USERS_FILE, "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print("Unable to load users, file not found!")
+
+
+def display_users(users: list[dict[str, Any]]) -> None:
+    """
+    Displays each user in ``users`` on a new line.
+
+    Args:
+        users (dict[str, Any]): List of user dictionaries.
+
+    Returns:
+        None
+    """
+    for user in users:
         print(user)
+
+
+def filter_users_by_name(name: str) -> None:
+    """
+    Filter users by name and display the results.
+
+    Args:
+        name (str): Name of the user to filter by.
+
+    Returns:
+        None
+    """
+    users = get_users()
+    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    display_users(filtered_users)
 
 
 def filter_users_by_age(age):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """
+    Filter users by age and display the results.
 
+    Args:
+        age (str): Age of the user to filter by.
+
+    Returns:
+        None
+    """
+    users = get_users()
     filtered_users = [user for user in users if user["age"] == int(age)]
-
-    for user in filtered_users:
-        print(user)
+    display_users(filtered_users)
 
 
 def filter_users_by_email(email):
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    """
+    Filter users by email and display the results.
 
+    Args:
+        email (str): Email of the user to filter by.
+
+    Returns:
+        None
+    """
+    users = get_users()
     filtered_users = [user for user in users if user["email"] == email]
-
-    for user in filtered_users:
-        print(user)
+    display_users(filtered_users)
 
 
-if __name__ == "__main__":
+def main():
     filter_option = input(
         "What would you like to filter by? (Currently, only "
         "'age', 'email' and 'name' is supported): ").strip().lower()
@@ -47,3 +97,7 @@ if __name__ == "__main__":
         filter_users_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+
+if __name__ == "__main__":
+    main()
